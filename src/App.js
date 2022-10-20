de077f65ae0206ad46f7a663d13954aa406ff058
JayRKyd <knowlesjordy@gmail.com>
2022-10-19 21:52:59 -0400

    small changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import {useCallback, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ const [qrImg, setQrCode] = useState("");
+ const [active, setQrCodeStatus] = useState("");
+
+const generateQR = useCallback((formEvent) => {
+  formEvent.preventDefault()
+  const qrValue = formEvent.target.elements['qr_code'].value.trim()
+  if(!qrValue) return
+  setQrCode(`http://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`)
+  setQrCodeStatus('active')
+}, []);
+
+const inputChange = useCallback(event => {
+  if(event.target.value === '') setQrCodeStatus('inActive');
+}, []);
+
+return (
+  <div className={ `wrapper ${active} === 'active' ? "wrapper active" : "wrapper" `}>
+    <header>
+      <h1>QR Code Generator</h1>
+      <p>Paste a URL or enter text to create QR code</p>
+    </header>
+    <form class="form" onSubmit={generateQR}>
+      <input onChange={inputChange} type="text" id="qr_code" name="qr_code" spellCheck="false" placeholder="Enter text or URL" />
+      <button type="submit">Generate QR Code</button>
+    </form>
+    <div class="qr-code">
+      <img src={qrImg} alt="qr-code" />
     </div>
-  );
+  </div>
+)
 }
 
 export default App;
